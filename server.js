fefc56d8cc089757fe32759270b4fa9116b8e8a7
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2021-11-02 15:02:48 +0100

    Add client part, add seats refresh

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(cors({
+  "origin": "http://localhost:3000",
+  "methods": "GET,POST,DELETE,PUT",
+}));
 
 const testimonialRoutes = require('./routes/testimonials.routes');
 const concertRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
-app.use('/', testimonialRoutes);
-app.use('/', concertRoutes);
-app.use('/', seatsRoutes);
+app.use('/api', testimonialRoutes);
+app.use('/api', concertRoutes);
+app.use('/api', seatsRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
