94792a4797ee99607de556b87dd38a39ad18b609
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2021-11-23 17:43:31 +0100

    Hide db user and password

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use((req, res) => {
 const NODE_ENV = process.env.NODE_ENV;
 let dbUri = '';
 
-if(NODE_ENV === 'production') dbUri = 'mongodb+srv://adminDB:adminDB@newwavefestival.izzmw.mongodb.net/NewWaveDB?retryWrites=true&w=majority';
+if(NODE_ENV === 'production') dbUri = `mongodb+srv://${process.env.dbUser}:${process.env.dbPass}@newwavefestival.izzmw.mongodb.net/NewWaveDB?retryWrites=true&w=majority`;
 else if(NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/NewWaveDBtest';
 else dbUri = 'mongodb://localhost:27017/NewWaveDB';
 
